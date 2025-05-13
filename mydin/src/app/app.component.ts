1afd04f9e2a09840c169f8caeb6869300ccc87ba
Lucca Zille <90798115+luccazille@users.noreply.github.com>
2025-05-13 20:03:27 -0300

    Iniciando projeto
    Iniciando projeto

---
+++ mydin/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./sharepage/navbar/navbar.component";
+import { FooterComponent } from "./sharepage/footer/footer.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'mydin';
+}
